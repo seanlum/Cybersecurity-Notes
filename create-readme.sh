066dacbfaa72bdfb10e7a0255a28dc8ee01655fe
kalanik0a <seanjklum@gmail.com>
2025-05-14 19:30:41 -0700

    Added basic exclusions to README.md

--- create-readme.sh
+++ create-readme.sh
@@ -1,2 +1,2 @@
 echo '# Index' > README.md
-find . -type f -name $"*.md" | awk '{ begin; gsub(/[ ]/, "%20",$0); print "- ["$0"]("$0")\n"; }' >> README.md
+find . -type f -name $"*.md" -not -path "./Changes/*" | awk '{ begin; gsub(/[ ]/, "%20",$0); print "- ["$0"]("$0")\n"; }' >> README.md
